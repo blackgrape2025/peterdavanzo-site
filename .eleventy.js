246e175d5f98bb31d7b93cc9c8b92687417e1528
blackgrape2025 <peterdavanzonz@gmail.com>
2026-05-29 17:33:00 +1200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,13 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("background.jpg");
-
+eleventyConfig.addFilter("readableDate", function(dateObj) {
+  return new Date(dateObj).toLocaleDateString("en-NZ", {
+    day: "numeric",
+    month: "long",
+    year: "numeric"
+  });
+});
   return {
     dir: {
       input: ".",

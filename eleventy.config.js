c00f645805bfb2056208100c4c849d586b3ebfc3
blackgrape2025 <peterdavanzonz@gmail.com>
2026-06-28 19:11:12 +1200

    Delete eleventy.config.js

--- eleventy.config.js
+++
@@ -1,9 +0,0 @@
-const sitemapPlugin = require("@quasibit/eleventy-plugin-sitemap");
-
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(sitemapPlugin, {
-    sitemap: {
-      hostname: "https://peterdavanzo.com"
-    }
-  });
-};
